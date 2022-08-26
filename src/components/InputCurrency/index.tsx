1382d8258b3df717c7efab206343df16fc0c3e2e
Arthur Borges <arthurborges@ufu.br>
2022-08-26 10:09:04 -0300

    refactor: improving input currency to work on mobile

--- src/components/InputCurrency/index.tsx
+++ src/components/InputCurrency/index.tsx
@@ -3,20 +3,20 @@ import { useState } from "react";
 export const InputCurrency = () => {
     const [value, setValue] = useState('');
 
-    function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
-        e.preventDefault();
+    function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+        event.preventDefault();
 
-        if ((/[0-9]+/g).test(e.key) || e.key === 'Backspace') {
+        if (event.key.replace(/\D/g, "") !== "" || event.key === 'Backspace') {
             let formattedValue: any = value.replace(/[^0-9]/g, '');
 
-            if (e.key === "Backspace") {
+            if (event.key === "Backspace") {
                 formattedValue = formattedValue.slice(0, -1);
                 formattedValue = formattedValue / 100;
                 setValue(formatMoney(formattedValue));
                 return
             }
-            if (value.length < 16) {
-                formattedValue = formattedValue + e.key;
+            if (event.key.replace(/\D/g, "") !== "" && value.length < 16) {
+                formattedValue = formattedValue + event.key;
                 formattedValue = formattedValue / 100;
                 setValue(formatMoney(formattedValue));
             }
